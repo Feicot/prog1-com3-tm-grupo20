70444bf79efaf3755abe245be1a35452497f85b3
FrnMelhem <163549478+FrnMelhem@users.noreply.github.com>
2024-06-23 11:05:22 -0300

    orga

--- js/home.js
+++ js/home.js
@@ -3,31 +3,26 @@
 window.addEventListener("load", function() {
         fetch("https://fakestoreapi.com/products/category/jewelery")
             .then(res=>res.json())
-            .then(json=>cargarh1(json))
+            .then(json=>cargarh("j",json))
             fetch("https://fakestoreapi.com/products/category/men's%20clothing")
             .then(res=>res.json())
-            .then(json=>cargarh2(json))
+            .then(json=>cargarh("h",json))
+            fetch("https://fakestoreapi.com/products/category/women's%20clothing")
+            .then(res=>res.json())
+            .then(json=>cargarh("m",json))
     })
 
-function cargarh1 (Lositems){
+function cargarh (prefijo,Lositems){
 for(j=0;j<=3;j++){
-    document.getElementById ("titulo"+(j+1)).innerHTML=Lositems[j].title;
-    document.getElementById ("descripcion"+(j+1)).innerHTML=Lositems[j].description;
-    document.getElementById ("precio"+(j+1)).innerHTML=Lositems[j].price;
-    document.getElementById ("foto"+(j+1)).src=Lositems[j].image;
+    document.getElementById ("titulo"+prefijo+(j+1)).innerHTML=Lositems[j].title;
+    document.getElementById ("descripcion"+prefijo+(j+1)).innerHTML=Lositems[j].description;
+    document.getElementById ("precio"+prefijo+(j+1)).innerHTML=Lositems[j].price;
+    document.getElementById ("foto"+prefijo+(j+1)).src=Lositems[j].image;
 }
 
 }
 
-function cargarh2 (Lositems){
-    for(j=0;j<=3;j++){
-        document.getElementById ("tituloh"+(j+1)).innerHTML=Lositems[j].title;
-        document.getElementById ("descripcionh"+(j+1)).innerHTML=Lositems[j].description;
-        document.getElementById ("precioh"+(j+1)).innerHTML=Lositems[j].price;
-        document.getElementById ("fotoh"+(j+1)).src=Lositems[j].image;
-    }
-    
-    }
+
 
 
 
