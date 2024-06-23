f8447277cba15128a75fd676d660924591f1444f
FrnMelhem <163549478+FrnMelhem@users.noreply.github.com>
2024-06-23 10:17:33 -0300

    limit js

--- js/home.js
+++ js/home.js
@@ -1,7 +1,7 @@
 
 
 window.addEventListener("load", function() {
-        fetch('https://fakestoreapi.com/products')
+        fetch("https://fakestoreapi.com/products/category/men's%20clothing")
             .then(res=>res.json())
             .then(json=>console.log(json))
     })
