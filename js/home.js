529a2029c11d1d6e2d9c76c5a168c043af664d4d
FrnMelhem <163549478+FrnMelhem@users.noreply.github.com>
2024-06-23 10:55:59 -0300

    secciones

--- js/home.js
+++ js/home.js
@@ -4,6 +4,9 @@ window.addEventListener("load", function() {
         fetch("https://fakestoreapi.com/products/category/jewelery")
             .then(res=>res.json())
             .then(json=>cargarh1(json))
+            fetch("https://fakestoreapi.com/products/category/men's%20clothing")
+            .then(res=>res.json())
+            .then(json=>cargarh2(json))
     })
 
 function cargarh1 (Lositems){
@@ -16,7 +19,15 @@ for(j=0;j<=3;j++){
 
 }
 
-
+function cargarh2 (Lositems){
+    for(j=0;j<=3;j++){
+        document.getElementById ("tituloh"+(j+1)).innerHTML=Lositems[j].title;
+        document.getElementById ("descripcionh"+(j+1)).innerHTML=Lositems[j].description;
+        document.getElementById ("precioh"+(j+1)).innerHTML=Lositems[j].price;
+        document.getElementById ("fotoh"+(j+1)).src=Lositems[j].image;
+    }
+    
+    }
 
 
 
