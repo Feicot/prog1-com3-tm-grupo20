5c5909d8644abcb158282745f13f2c1fb1c38ac5
FrnMelhem <163549478+FrnMelhem@users.noreply.github.com>
2024-06-23 10:45:28 -0300

    sss

--- js/home.js
+++ js/home.js
@@ -9,6 +9,9 @@ window.addEventListener("load", function() {
 function cargarh1 (Lositems){
 for(j=0;j<=3;j++){
     document.getElementById ("titulo"+(j+1)).innerHTML=Lositems[j].title;
+    document.getElementById ("descripcion"+(j+1)).innerHTML=Lositems[j].description;
+    document.getElementById ("precio"+(j+1)).innerHTML=Lositems[j].price;
+    document.getElementById ("foto"+(j+1)).src=Lositems[j].image;
 }
 
 }
