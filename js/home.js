8dfc5bcd1c40285ef2e78ba61acb62d23326271f
FrnMelhem <163549478+FrnMelhem@users.noreply.github.com>
2024-06-23 10:14:30 -0300

    js

--- js/home.js
+++ js/home.js
@@ -1,19 +1,14 @@
-let articulo1 ={
-    nombre:"Chaqueta con estampado Gucci X The North Face",
-    precio:"$2500",
-    descripcion:"jksjfksskndskkfsn",
-    foto:"../img/camperaG2.png.png",
 
-};
 
-let articulo2 ={
-    nombre:"Chaqueta con estampado Gucci X The North Face",
-    precio:"$3200",
-    descripcion:"jksjfksskndskkfsn",
-    foto:"../img/camperaG2.png.png",
+window.addEventListener("load", function() {
+        fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=>console.log(json))
+    })
 
-};
 
-let productos = [articulo1,articulo2];
-let productosstr = JSON.stringify (productos);
-localStorage.setItem("productoslocal",productosstr);
+
+
+
+
+
