2def95e5f9012162d23d041b274b48cdabff4ac7
altube <taltube@udesa.edu.ar>
2024-06-24 22:50:15 -0300

    arreglo

--- js/register.js
+++ js/register.js
@@ -34,7 +34,7 @@ window.onload = function() {
                 formIsValid = false;
             }
             if (formIsValid) {
-                window.location.href = "index.html";
+                window.location.href = "inicio-sesion.html";
             }
         });
     } else {
