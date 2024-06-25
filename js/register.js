0b9ac8b6d2504385dfb1ef77b4172da8914d2d86
altube <taltube@udesa.edu.ar>
2024-06-24 21:58:41 -0300

    arregle register para que ande

--- js/register.js
+++ js/register.js
@@ -5,38 +5,38 @@ window.onload = function() {
         form.addEventListener('submit', function(event) {
             event.preventDefault();
 
-            const username = document.getElementById('username').value;
-            const email = document.getElementById('email').value;
-            const password = document.getElementById('password').value;
-            const confirmPassword = document.getElementById('confirm_password').value;
-            document.getElementById('emailError').innerText = '';
-            document.getElementById('passwordError').innerText = '';
-            document.getElementById('confirmPasswordError').innerText = '';
+            const username = document.getElementById("username").value;
+            const email = document.getElementById("email").value;
+            const password = document.getElementById("password").value;
+            const confirmPassword = document.getElementById("confirm_password").value;
+            document.getElementById("emailError").innerText = '';
+            document.getElementById("passwordError").innerText = '';
+            document.getElementById("confirmPasswordError").innerText = '';
             let formIsValid = true;
             if (username.trim() === '') {
-                document.getElementById('usernameError').innerText = 'Por favor complete el campo';
+                document.getElementById("usernameError").innerText = "Por favor complete el campo";
                 formIsValid = false;
             }
             if (email.trim() === '') {
-                document.getElementById('emailError').innerText = 'Por favor complete el campo';
+                document.getElementById("emailError").innerText = "Por favor complete el campo";
                 formIsValid = false;
             }
             if (password.trim() === '') {
-                document.getElementById('passwordError').innerText = 'Por favor complete el campo';
+                document.getElementById("passwordError").innerText = 'Por favor complete el campo';
                 formIsValid = false;
             } else if (password.length < 6) {
-                document.getElementById('passwordError').innerText = 'Debe ingresar al menos 6 caracteres';
+                document.getElementById("passwordError").innerText = "Debe ingresar al menos 6 caracteres";
                 formIsValid = false;
             }
             if (password !== confirmPassword) {
-                document.getElementById('confirmPasswordError').innerText = 'Las contraseñas no coinciden';
+                document.getElementById("confirmPasswordError").innerText = "Las contraseñas no coinciden";
                 formIsValid = false;
             }
             if (formIsValid) {
-                window.location.href = 'inicio-sesion.html';
+                window.location.href = "inicio-sesion.html";
             }
         });
     } else {
         console.error("Formulario no encontrado.");
     }
-}
+};
