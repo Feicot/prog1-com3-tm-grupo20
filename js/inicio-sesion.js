a84f909c042822ea1998fc6766583004f5e5c941
altube <taltube@udesa.edu.ar>
2024-06-24 21:41:01 -0300

    cambie link con forgot password

--- js/inicio-sesion.js
+++ js/inicio-sesion.js
@@ -7,7 +7,6 @@ window.onload = function() {
 
             const email = document.getElementById('email').value;
             const password = document.getElementById('password').value;
-
             if (email.trim() === '') {
                 alert('Por favor complete el campo email');
                 return;
